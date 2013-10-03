1a098cec9f4c2ff39a8b2752e29f8c72c01bed96
Alexis Matherlee <alexismatherlee@gmail.com>
2013-10-03 11:12:04 -0700

    added 5 turn max

--- My First Project/main.m
+++ My First Project/main.m
@@ -12,10 +12,42 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        answer = arc4random() % 100 + 1;
+        turn = 1;
         
+        
+        while (guess != answer && turn <= 5) {
+        
+            NSLog(@"Guess #%i: Enter a number between 1 and 100", turn);
+            scanf("%i", &guess);
+            
+            
+        if (guess != answer && turn < 5) {
+            
+            if (guess > answer) {
+                NSLog(@"Lower!");
+            }
+            else if (guess < answer) {
+                NSLog(@"Higher!");
+            }
+        }
+        else if (guess != answer && turn >= 5) {
+
+            NSLog(@"Sorry! The number was %i! Play again!", answer);
+                }
+        else {
+            NSLog(@"Correct! The answer was %i", answer);
+            }
+        turn++;
+        } // end of while loop
+        
+        if (guess == answer) {
+            NSLog(@"It took you %i tries", turn - 1 );
+        }
     }
     return 0;
 }
